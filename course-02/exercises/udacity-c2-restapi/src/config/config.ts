66c74ea8027198ffbf3b969518ae1d46fabff20d
SuryankDixit <suryankdixit@gmail.com>
2020-07-20 22:47:03 +0530

    Modified.

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "centigramdev",
+    "password": "centigramdev",
+    "database": "centigramdev",
+    "host": "centigramdev.c10sm55op6wi.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    // "aws_region": "us-east-2",
+    // "aws_profile": "default",
+    // "aws_media_bucket": "udagram-ruttner-dev"
   },
   "prod": {
     "username": "",
